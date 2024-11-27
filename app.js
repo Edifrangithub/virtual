357f7e7c6eb301eb129061331eeeb264589d8b79
Edifrangithub <163612042+Edifrangithub@users.noreply.github.com>
2024-11-27 13:46:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three.module.js';  // Ajuste o caminho
+import * as THREE from './three.js-dev/build/three.module.js';// Ajuste o caminho
 
 // Configuração inicial
 const scene = new THREE.Scene();
